d602811aa68f880eac6c8eb2f33c78358b0ed422
Luis Concha <lconcha@gmail.com>
2025-06-23 19:21:06 -0600

    can now write tsf for par and perp

--- cmd/fixel2tsf_parperp.cpp
+++ cmd/fixel2tsf_parperp.cpp
@@ -30,6 +30,7 @@
 #include "dwi/tractography/mapping/loader.h"
 #include "dwi/tractography/mapping/mapper.h"
 
+
 using namespace MR;
 using namespace App;
 
@@ -57,8 +58,10 @@ void usage ()
                      "between a fixel direction and track tangent "
                      "(default = " + str(DEFAULT_ANGULAR_THRESHOLD, 2) + " degrees)")
   + Argument ("value").type_float (0.001, 90.0)
-  + Option ("perp", "output TSF file for most perpendicular fixel").argument ("filename").type_file_out ()
-  + Option ("avg", "output TSF file for average of all except most parallel fixel").argument ("filename").type_file_out ();
+  + Option ("tsf_perp", "optional output track scalar file for the most perpendicular fixel values")
+  + Argument ("tsf_perp").type_file_out ()
+  + Option ("tsf_perpav", "output tsf file for average of all except most parallel fixel")
+  + Argument ("tsf_perpav").type_file_out ();
 }
 
 using SetVoxelDir = DWI::Tractography::Mapping::SetVoxelDir;
@@ -82,13 +85,21 @@ void run ()
 
   DWI::Tractography::ScalarWriter<float> tsf_writer (argument[2], properties);
 
+  // Optional perpendicular TSF writer
   std::unique_ptr<DWI::Tractography::ScalarWriter<float>> tsf_writer_perp;
-  std::unique_ptr<DWI::Tractography::ScalarWriter<float>> tsf_writer_avg;
-  if (has_option("perp"))
-    tsf_writer_perp.reset(new DWI::Tractography::ScalarWriter<float>(get_option_value("perp"), properties));
-  if (has_option("avg"))
-    tsf_writer_avg.reset(new DWI::Tractography::ScalarWriter<float>(get_option_value("avg"), properties));
+  std::string tsf_perp_filename = get_option_value("tsf_perp", std::string());
+  if (!tsf_perp_filename.empty()) {
+    tsf_writer_perp.reset(new DWI::Tractography::ScalarWriter<float>(tsf_perp_filename, properties));
+  }
 
+  // Optional perpendicular_av TSF writer
+  std::unique_ptr<DWI::Tractography::ScalarWriter<float>> tsf_writer_perpav;
+  std::string tsf_perpav_filename = get_option_value("tsf_perpav", std::string());
+  if (!tsf_perpav_filename.empty()) {
+    tsf_writer_perpav.reset(new DWI::Tractography::ScalarWriter<float>(tsf_perpav_filename, properties));
+  }
+
+  
   float angular_threshold = get_option_value ("angle", DEFAULT_ANGULAR_THRESHOLD);
   const float angular_threshold_dp = cos (angular_threshold * (Math::pi / 180.0));
 
@@ -117,14 +128,16 @@ void run ()
       scalars_perp.set_index (tck.get_index());
       scalars_perp.resize (tck.size(), 0.0f);
     }
-    if (tsf_writer_avg) {
+    if (tsf_writer_perpav) {
       scalars_avg.clear();
       scalars_avg.set_index (tck.get_index());
       scalars_avg.resize (tck.size(), 0.0f);
     }
 
     for (size_t p = 0; p < tck.size(); ++p) {
+      std::cout << "Processing point " << p << " of " << tck.size()-1 << std::endl;
       voxel_pos = transform.scanner2voxel * tck[p].cast<default_type> ();
+      std::cout << "  Voxel position: " << voxel_pos.transpose() << std::endl;
       for (SetVoxelDir::const_iterator d = dixels.begin(); d != dixels.end(); ++d) {
         if ((int)round(voxel_pos[0]) == (*d)[0] && (int)round(voxel_pos[1]) == (*d)[1] && (int)round(voxel_pos[2]) == (*d)[2]) {
           assign_pos_of (*d).to (in_index_image);
@@ -137,12 +150,16 @@ void run ()
 
           in_index_image.index(3) = 0;
           index_type num_fixels_in_voxel = in_index_image.value();
+          std::cout << "  Number of fixels in voxel: " << num_fixels_in_voxel << std::endl;
           in_index_image.index(3) = 1;
           index_type offset = in_index_image.value();
 
           for (size_t fixel = 0; fixel < num_fixels_in_voxel; ++fixel) {
             in_directions_image.index(0) = offset + fixel;
             const float dp = abs (dir.dot (Eigen::Vector3f (in_directions_image.row(1))));
+            in_data_image.index(0) = offset + fixel;
+            const float value = in_data_image.value();
+            std::cout << "    Fixel index: " << fixel << ", dp: " << dp << ", scalar:" << value << std::endl;
             if (dp > largest_dp) {
               largest_dp = dp;
               closest_fixel_index = fixel;
@@ -152,47 +169,57 @@ void run ()
               perp_fixel_index = fixel;
             }
           }
+
+          std::cout << "  Largest dp: " << largest_dp << ", closest fixel index: " << closest_fixel_index
+                    << ", most perpendicular fixel index: " << perp_fixel_index << std::endl;
           // Most parallel
           if (largest_dp > angular_threshold_dp) {
             in_data_image.index(0) = offset + closest_fixel_index;
             const float value = in_data_image.value();
-            if (std::isfinite (value))
+            if (std::isfinite (value)) {
+              std::cout << "  Parallel scalar value is: " << value  << std::endl;
               scalars[p] = value;
-            else
-              scalars[p] = 0.0f;
+            } else {
+              scalars[p] = NAN;
+            }
           } else {
-            scalars[p] = 0.0f;
+            std::cout << "  [WARN] Largest dp below threshold (" << angular_threshold_dp << "), setting parallel scalar to NAN." << std::endl;
+            scalars[p] = NAN;
           }
           // Most perpendicular
           if (tsf_writer_perp) {
-            if (perp_fixel_index >= 0) {
-              in_data_image.index(0) = offset + perp_fixel_index;
-              const float value_perp = in_data_image.value();
-              if (std::isfinite (value_perp))
-                scalars_perp[p] = value_perp;
-              else
-                scalars_perp[p] = 0.0f;
+            if (num_fixels_in_voxel == 1) {
+              // If there's only one fixel, we can directly assign it
+              scalars_perp[p] = NaN;
+              std::cout << "  [WARN] Only one fixel found, setting perp to NAN." << std::endl;
             } else {
-              scalars_perp[p] = 0.0f;
+                in_data_image.index(0) = offset + perp_fixel_index;
+                const float value_perp = in_data_image.value();
+                scalars_perp[p] = value_perp;
+                std::cout << "  Perpendicular fixel value: " << value_perp << std::endl;
             }
           }
           // Average of all except most parallel
-          if (tsf_writer_avg) {
-            float sum = 0.0f;
-            int count = 0;
-            for (size_t fixel = 0; fixel < num_fixels_in_voxel; ++fixel) {
-              if ((int)fixel == closest_fixel_index) continue;
-              in_data_image.index(0) = offset + fixel;
-              float v = in_data_image.value();
-              if (std::isfinite(v)) {
-                sum += v;
-                ++count;
+          if (tsf_writer_perpav) {
+            if (num_fixels_in_voxel == 1) {
+              // If there's only one fixel, we can directly assign it
+              scalars_avg[p] = NaN;
+              std::cout << "  [WARN] Only one fixel found, setting perpav to NAN." << std::endl;
+            } else {
+              float sum = 0.0f;
+              int count = 0;
+              for (size_t fixel = 0; fixel < num_fixels_in_voxel; ++fixel) {
+                if ((int)fixel == closest_fixel_index) continue;
+                in_data_image.index(0) = offset + fixel;
+                float v = in_data_image.value();
+                if (std::isfinite(v)) {
+                  sum += v;
+                  ++count;
+                }
               }
+              scalars_avg[p] = sum / count;    
+              std::cout << "  Average of all except most parallel fixel: " << scalars_avg[p] << std::endl;          
             }
-            if (count > 0)
-              scalars_avg[p] = sum / count;
-            else
-              scalars_avg[p] = 0.0f;
           }
           break;
         }
@@ -200,7 +227,7 @@ void run ()
     }
     tsf_writer (scalars);
     if (tsf_writer_perp) (*tsf_writer_perp)(scalars_perp);
-    if (tsf_writer_avg) (*tsf_writer_avg)(scalars_avg);
+    if (tsf_writer_perpav) (*tsf_writer_perpav)(scalars_avg);
     progress++;
   }
 }
